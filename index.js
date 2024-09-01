13d6d0aaaccbc44b7a263a2dc35791a8c39052b9
ALNoman78 <abdullaalno078@gmail.com>
2024-09-01 18:32:32 +0600

    new file added

---
+++ index.js
@@ -0,0 +1,32 @@
+let x = [12, 13, 15, 156, 65, 34, 76, 44];
+let y = x.includes(12);
+console.log(y);
+
+let m = x.indexOf(65);
+console.log(m);
+
+
+let math = 11;
+let sub = 3;
+let totalValue = math / sub;
+let convertNumber = parseFloat(totalValue.toFixed(2))
+console.log(convertNumber);
+
+// let zx = 30;
+// if (zx > 4) {
+//     console.log(true)
+// }else{
+//     console.log(false);
+// }
+// console.log(zx);
+
+// how to multiple a value
+
+let d = 3;
+let suq = d ** 3
+console.log(suq);
+
+let modus = 10
+let maths = 3;
+let total = modus % maths
+console.log(total);
